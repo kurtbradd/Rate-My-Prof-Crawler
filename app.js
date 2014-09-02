4e8e1f27e86f45920cec2784a1c9ee0213814b51
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-01 20:36:57 -0400

    static assets before routes in pipeline

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
+// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {

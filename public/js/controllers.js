3411ad266133e8c0f236d1529d9802a3cf096ad9
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-01 21:40:16 -0400

    added socket.io on the client

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,6 +4,6 @@ module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review)
 	
 	console.log('cntl loaded');
 	//in production use actual host name
-	// var socket = io.connect('http://localhost');
+	var socket = io.connect('http://localhost');
   
 }])

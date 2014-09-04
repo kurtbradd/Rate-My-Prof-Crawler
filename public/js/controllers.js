39babae5ca556141cb277ba80947f6e8d5233c11
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-04 19:07:22 -0400

    updated controller logic and log statements

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,13 +2,37 @@ var module = angular.module('MyApp.controllers', []);
 
 module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review){
 	
-	console.log('cntl loaded');
-	getReviews();
+	console.log('Controller Loaded');
+
 	//in production use actual host name
 	var socket = io.connect('http://localhost');
+	getReviews();
+
+	$scope.reviews = [];
+	$scope.crawlProgress = 0;
 
 	socket.on('connect', function () {
 		console.log('socket io connected');
+	})
+
+	socket.on('crawlComplete', function () {
+		console.log('Crawl Complete');
+		$scope.crawlProgress = 0;
+		$scope.$apply();
+		window.alert('Completed');
+	})
+
+	socket.on('crawlFailed', function (error) {
+		console.log('Crawl Failed with Error: ' + error);
+		$scope.crawlProgress = 0;
+		$scope.$apply();
+		window.alert(error || 'Something went wrong! :(');
+	})
+
+	socket.on('crawlProgress', function (progress) {
+		$scope.crawlProgress = progress;
+		$scope.$apply();
+		console.log(progress);
 	}) 
 
 	$scope.formData = {
@@ -21,22 +45,30 @@ module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review)
 	$scope.submit = function () {
 		Review.submit($scope.formData)
 		.success(function(data) {
-			// clearForm();
-			console.log(data);
+			clearForm();
 		})
 		.error(function(error) {
-
+			window.alert("Please make sure form filled correctly.");
 		})
 	}
 
 	function getReviews () {
 		Review.getReviews()
 		.success(function(data) {
-			console.log(data);
+			$scope.reviews = data.reverse();
 		})
 		.error(function(error) {
-			
+			window.alert('Looks like something went wrong, lets try that again');
 		})
+	}
+
+	$scope.percentComplete = function () {
+		return "width: " + $scope.crawlProgress + "%;";
+	};
+
+	$scope.dateToString = function (date) {
+		date = new Date(date);
+		return date.toDateString() + " @ " +date.toLocaleTimeString()
 	}
 
 	function clearForm () {

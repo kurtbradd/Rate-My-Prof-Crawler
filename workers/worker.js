c73f4ea384396d4ed846721c17c5b1f0a032d0b5
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-03 23:21:52 -0400

    job crawling implemented

--- workers/worker.js
+++ workers/worker.js
@@ -7,33 +7,84 @@ if (cluster.isMaster) {
 	}
 }
 else {
-	var	kue 	= require("kue");
-	var	jobs 	= kue.createQueue();
+	console.log("Cluster: " + cluster.worker.id + " - ready.");
+	// var	kue 				= require("kue");
+	var kue 				= require("kue-send");
+	var	jobs 				= kue.createQueue();
+	var	jobsTwo 		= kue.createQueue();
+	var rateMyProf 	= require('./rateMyProf.js');
+	var Q 					= require('q');
+
 
 	jobs.process('crawlProfessor', 1, function (job, done){
-		console.log('got crawlProfessor, finished job');
+		// console.log('got crawlProfessor, finished job');
+		// console.log(cluster.worker.id);
 
-		// in here create the need for new jobs;
+		var infoPromise = rateMyProf.crawlProfessorInfo(job.data.url);
+		var urlsPromise = rateMyProf.createPaginationUrls(job.data.url);
 
-		var job = jobs.create('crawlProfessorReviews', {});
-		job
-		.on('complete', function (){
-			done()
+		// after prof info and urls recieved
+		Q.all([infoPromise, urlsPromise])
+		.then(function (data) {
+
+			var profInfo = data[0];
+			var urls = data[1];
+			
+			var reviews = []
+			var jobPromises = [];
+
+			//create promises for each url to return
+			urls.forEach(function(url) {
+				var deferredJob = Q.defer();
+				var crawl_job = jobs.create('crawlProfessorReviews', {'url': url});
+				crawl_job.delay(100);
+				jobPromises.push(deferredJob.promise);
+				crawl_job.on('result', function (reviewsArray) {
+					reviews = reviews.concat(reviewsArray);
+					deferredJob.resolve();
+				})
+				crawl_job.on('failed attempt', function () {
+					console.log('Failed Crawl Job Attempt')
+				})
+				crawl_job.on('failed', function () {
+					deferredJob.reject();
+				})
+				crawl_job.save();
+			})
+
+			console.log('here');
+			Q.all(jobPromises)
+			.then(function(data) {
+				// all reviews should be here
+				console.log(jobPromises.length);
+				console.log(reviews.length);
+				done()
+			})
+			.fail(function (error) {
+				done(error);
+			})
+
 		})
-		job.save();
+		.fail(function (error) {
+			done(error)
+		})
 	});
 
-	jobs.process('crawlProfessorReviews', 1, function (job, done){
-		console.log('got crawlProfessorReviews, finished job');
-		done()
+	jobsTwo.process('crawlProfessorReviews', 1, function (job, done){
+		rateMyProf.crawlReviews(job.data.url)
+		.then(function (data) {
+			job.send("result", data);
+			done();
+		})
+		.fail(function (error) {
+			done(error);
+		})
 	});
-
 }
 
-
 process.once( 'SIGINT', function (sig) {
-  jobs.shutdown(function(err) {
+	jobs.shutdown(function(err) {
 		console.log( 'Kue is shut down.', err||'' );
 		process.exit( 0 );
-  }, 5000 );
+	}, 5000 );
 });

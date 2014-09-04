893cc0ed38a385c1efb939d1eb98e8279a047575
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-03 23:22:15 -0400

    debug changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,7 +3,7 @@ var module = angular.module('MyApp.controllers', []);
 module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review){
 	
 	console.log('cntl loaded');
-	
+	getReviews();
 	//in production use actual host name
 	var socket = io.connect('http://localhost');
 
@@ -12,14 +12,17 @@ module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review)
 	}) 
 
 	$scope.formData = {
-		name: "",
-		url: "",
+		// name: null,
+		// url: null,
+		name: "Richard B Day",
+		url: "http://www.ratemyprofessors.com/ShowRatings.jsp?tid=10844",
 	}
 	
 	$scope.submit = function () {
 		Review.submit($scope.formData)
 		.success(function(data) {
-			clearForm();
+			// clearForm();
+			console.log(data);
 		})
 		.error(function(error) {
 
@@ -29,7 +32,7 @@ module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review)
 	function getReviews () {
 		Review.getReviews()
 		.success(function(data) {
-
+			console.log(data);
 		})
 		.error(function(error) {
 			
@@ -37,8 +40,8 @@ module.controller('DashboardCtrl', ['$scope', 'Review', function($scope, Review)
 	}
 
 	function clearForm () {
-		$scope.formData.name = "";
-		$scope.formData.url = "";
+		$scope.formData.name = null;
+		$scope.formData.url = null;
 	}
 
 }])

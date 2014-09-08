9b0662fe8b37e89071ede756effcf6b80c0bdb0d
Kurt DaCosta <kurtbradd@gmail.com>
2014-09-08 15:32:03 -0400

    implemented batch scaffolding

---
+++ controllers/BatchController.js
@@ -0,0 +1,14 @@
+// csv parse
+// file reader
+// 
+
+module.exports = {
+	batchCrawl: function (req, res) {
+		/*
+		make sure file is uploaded from client
+		validate each url and professor
+		create jobs
+		socket.io overall job status
+		*/
+	}	
+}
